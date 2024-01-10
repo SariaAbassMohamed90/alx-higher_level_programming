f9a4f9961879824ba17be95ac25525d55b960d8a
SariaAbassMohamed90 <saryaabass90@gmail.com>
2024-01-10 09:55:19 -0800

    Updated

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Task 11 module'''
 
+
 class Student:
     '''student class'''
     def __init__(self, first_name, last_name, age):
@@ -8,23 +9,24 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         '''Return a dictionary representation of a student instance
         If attrs is a list of strings, only attribute names contained
         in this list must be retrieved.
         Otherwise, all attributes must be retrieved
         '''
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except Exception:
-            return self.__dict__
-        my_dict = dict()
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                my_dict[key] = value
+
+    try:
+        for attr in attrs:
+            if type(attr) is not str:
+                return self.__dict__
+    except Exception:
+        return self.__dict__
+    my_dict = dict()
+    for key, value in self.__dict__.items():
+        if key in attrs:
+            my_dict[key] = value
         return my_dict
 
     def reload_from_json(self, json):
