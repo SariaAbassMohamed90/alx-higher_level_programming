8d365b3764cf1755ef7f0690e8c9a227c62164f9
SariaAbassMohamed90 <saryaabass90@gmail.com>
2024-01-10 08:49:46 -0800

    Updated

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """defining save_to_json_file function"""
 
+
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    """Writes an object to text file"""
+    """writes an object to text file"""
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
