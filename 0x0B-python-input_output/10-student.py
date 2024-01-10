496f8594d2f0febd153251d342bd2f0e794ce5b9
SariaAbassMohamed90 <saryaabass90@gmail.com>
2024-01-10 09:46:52 -0800

    Updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,6 +16,7 @@ class Student:
         in this list must be retrieved
         Otherwise, all attributes must be retrieved
         '''
+
     try:
         for attr in attrs:
             if type(attr) is not str:
@@ -26,4 +27,4 @@ class Student:
     for key, value in self.__dict__.items():
         if key in attrs:
             my_dict[key] = value
-            return my_dict
+        return my_dict
