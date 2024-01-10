d9aceea5de949286dcdc10fb656b38eaf1bcd2f4
SariaAbassMohamed90 <saryaabass90@gmail.com>
2024-01-10 10:48:51 -0800

    Updated

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,11 +11,11 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        '''Return a dictionary representation of a student instance
-        If attrs is a list of strings, only attribute names contained
-        in this list must be retrieved.
-        Otherwise, all attributes must be retrieved
-        '''
+    '''Return a dictionary representation of a student instance
+    If attrs is a list of strings, only attribute names contained
+    in this list must be retrieved.
+    Otherwise, all attributes must be retrieved
+    '''
 
     try:
         for attr in attrs:
@@ -30,9 +30,10 @@ class Student:
         return my_dict
 
     def reload_from_json(self, json):
-        '''this function replaces all attributes of the student instance
-        with the ones in the json argument
-        '''
-        for key, value in json.items():
-            if key in self.__dict__:
-                self.__dict__[key] = value
+    '''this function replaces all attributes of the student instance
+    with the ones in the json argument
+    '''
+
+    for key, value in json.items():
+        if key in self.__dict__:
+            self.__dict__[key] = value
