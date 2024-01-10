513131e9723577a2e51733015685331e7bd47017
SariaAbassMohamed90 <saryaabass90@gmail.com>
2024-01-10 10:07:19 -0800

    Updated

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,6 +15,6 @@ def pascal_triangle(n):
         tmp = [1]
         for i in rang(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+            tmp.append(1)
+            triangles.append(tmp)
+            return triangles
